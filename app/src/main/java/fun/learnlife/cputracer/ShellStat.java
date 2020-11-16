83466503252948ba277098b788f7ece8f014b7c6
luochunyou <1104128773@qq.com>
2020-11-16 20:26:07 +0800

    .

--- app/src/main/java/fun/learnlife/cputracer/ShellStat.java
+++ app/src/main/java/fun/learnlife/cputracer/ShellStat.java
@@ -1,30 +1,9 @@
 package fun.learnlife.cputracer;
 
-import android.text.TextUtils;
-import android.util.Log;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-
 public class ShellStat extends IStat {
-    private static final int index_ratio = 3;
-    private static final int index_name = 9;
-    private static ArrayList<IStat> threadStats = new ArrayList<>();
-    private static IStat processStat;
     public String ratioStr;
     public String threadName;
     public String pidStr;
-
-    public static ArrayList<IStat> getThreadStats() {
-        return threadStats;
-    }
-
-    public static IStat getProcessStat() {
-        return processStat;
-    }
-
     @Override
     void updateCpuInfo() {
 
